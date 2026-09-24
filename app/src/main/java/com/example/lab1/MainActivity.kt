d9c87a82578dd85880892981c119fc692553c1a5
oio228 <yurii.bat@knf.stud.vu.lt>
2026-09-24 15:23:13 -0700

    Revert "Add comment for revert"
    This reverts commit bc72eb9880fe5fb1e4fcd0feb05361f9bc96b969.

--- app/src/main/java/com/example/lab1/MainActivity.kt
+++ app/src/main/java/com/example/lab1/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //Code for revert
+
         val root = findViewById<LinearLayout>(R.id.main)
         val textView = findViewById<TextView>(R.id.textView)
         val btnChangeText = findViewById<Button>(R.id.btnChangeText)
