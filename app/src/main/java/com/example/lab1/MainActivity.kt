7ee8ebc697e48b8531ccc5398debd60bebd16c08
oio228 <yurii.bat@knf.stud.vu.lt>
2026-09-24 15:12:50 -0700

    background color button

--- app/src/main/java/com/example/lab1/MainActivity.kt
+++ app/src/main/java/com/example/lab1/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.lab1
 
+import android.graphics.Color
 import android.os.Bundle
 import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -11,11 +13,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val root = findViewById<LinearLayout>(R.id.main)
         val textView = findViewById<TextView>(R.id.textView)
         val btnChangeText = findViewById<Button>(R.id.btnChangeText)
+        val btnChangeColor = findViewById<Button>(R.id.btnChangeColor)
+        val btnChangeBg = findViewById<Button>(R.id.btnChangeBg)
 
         btnChangeText.setOnClickListener {
             textView.text = "button was clicked"
+        }
+
+        btnChangeColor.setOnClickListener {
+            textView.setTextColor(Color.RED)
+        }
+
+        btnChangeBg.setOnClickListener {
+            root.setBackgroundColor(Color.YELLOW)
         }
     }
 }
